983a8a08376b2d5486c6d776814598f9a272b2ff
AJking144 <abdullah152574@gmail.com>
2024-05-22 05:40:10 -0700

    Add files via upload

---
+++ AJ.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum,product,avg;
+	printf("pls enter any two number");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	avg=sum/5;
+	product=a*b;
+	printf("sum of number are=%d\n",sum);
+	printf("avg of numberi is=%d\n",avg);
+	printf("product of number is=%d",product);
+}
